0eb39b3719f0bb2273c33a6c65c5c36a54229028
5urafel <surafel5540f@gmail.com>
2025-09-24 05:53:38 +0300

    feat: add login screen with validation

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+  String email = '';
+  String password = '';
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Form(
+        key: formKey,
+        child: Column(
+          children: [
+            Container(margin: EdgeInsets.only(top: 25.0)),
+            emailField(),
+            passwordField(),
+            Container(margin: EdgeInsets.only(top: 25.0)),
+            submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        label: Text('Email'),
+        hintText: 'you@example.com',
+      ),
+      validator: (value) {
+        if (value == null || !value.contains('@')) {
+          return 'invalid email address';
+        }
+      },
+      onSaved: (newValue) {
+        email = newValue ?? '';
+      },
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      keyboardType: TextInputType.visiblePassword,
+      obscureText: true,
+      decoration: InputDecoration(
+        label: Text('Password'),
+        hintText: 'password',
+      ),
+      validator: (value) {
+        if (value == null || value.length < 4) {
+          return 'password must at least be 4';
+        }
+      },
+      onSaved: (newValue) {
+        password = newValue ?? '';
+      },
+    );
+  }
+
+  Widget submitButton() {
+    return ElevatedButton(
+      onPressed: () {
+        if (formKey.currentState != null && formKey.currentState!.validate()) {
+          formKey.currentState!.save();
+          print('Time to send $email and $password to an API');
+        }
+      },
+      child: Text('Submit!'),
+    );
+  }
+}
